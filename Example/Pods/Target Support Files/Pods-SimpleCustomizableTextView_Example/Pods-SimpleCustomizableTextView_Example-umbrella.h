8048c13e3e0b9232b269ce296a1bbfbc72e8e47d
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-11-09 10:37:00 +0900

    create pod template

---
+++ Example/Pods/Target Support Files/Pods-SimpleCustomizableTextView_Example/Pods-SimpleCustomizableTextView_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SimpleCustomizableTextView_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SimpleCustomizableTextView_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/SimpleCustomizableTextView/SimpleCustomizableTextView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SimpleCustomizableTextView : NSObject
+@end
+@implementation PodsDummy_SimpleCustomizableTextView
+@end
